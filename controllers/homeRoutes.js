e48420750170183e5fd92ed49037ba2c7d08b06b
Mamba <84301026+lsoth@users.noreply.github.com>
2021-08-19 20:17:20 -0700

    fixed homeroutes that were having issues, as well as the models having trouble seeding

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,13 +3,13 @@ const path = require('path');
 const Workout = require('../models/Workout.js');
 
 router.get('/',(req, res)=> {
-    res.sendFile.apply(path.join(__dirname, '../public/index.html'))
+    res.sendFile(path.join(__dirname, '../public/index.html'))
 });
 router.get('/stats',(req, res)=> {
-    res.sendFile.apply(path.join(__dirname, '../public/stats.html'))
+    res.sendFile(path.join(__dirname, '../public/stats.html'))
 });
 router.get('/exercise',(req, res)=> {
-    res.sendFile.apply(path.join(__dirname, '../public/index.html'))
+    res.sendFile(path.join(__dirname, '../public/exercise.html'))
 });
 
 module.exports = router;
