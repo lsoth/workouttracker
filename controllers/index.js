fc13900fc95f3d74dffd75a16ec0a3a3908c4a14
Mamba <84301026+lsoth@users.noreply.github.com>
2021-08-19 23:34:56 -0700

    update routes to debug heroku

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,17 @@
 const router = require('express').Router();
 const workoutApi = require('./workoutApi');
-const homeRoutes = require('./homeRoutes');
+const path = require('path');
 
 router.use('/api', workoutApi);
-router.use('/', homeRoutes);
+
+router.get('/',(req, res)=> {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+});
+router.get('/stats',(req, res)=> {
+    res.sendFile(path.join(__dirname, '../public/stats.html'))
+});
+router.get('/exercise',(req, res)=> {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'))
+});
 
 module.exports = router;
