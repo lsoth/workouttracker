7c9ed4c79e92d8f94492215c4a366c4a912b7109
Mamba <84301026+lsoth@users.noreply.github.com>
2021-08-19 19:02:01 -0700

    homepage starts

--- controllers/workoutApi.js
+++ controllers/workoutApi.js
@@ -2,18 +2,19 @@ const router = require('express').Router();
 const db = require('../models')
 
 // get most recent workout and aggregate sums total duration
-router.get('/', async(req, res) => {
-    const lastWorkout = await db.Workout.aggregate([
-        {
-            $sort:{day:-1}
-        },
-        {
-            $addFields:{ totalDuration: {$sum = '$exercises.duration'}}
-        }
-    ]);
+// router.get('/', async(req, res) => {
+//     const lastWorkout = await db.Workout.aggregate([
+//         {
+//             $sort:{day:-1}
+//         },
+//         {
+//             $addFields:{ 
+//                 totalDuration: {$sum = '$exercises.duration'}}
+//         }
+//     ]);
 
-    res.json(lastWorkout)
+//     res.json(lastWorkout)
 
-});
+// });
 
-router.get('/range')
+// router.get('/range')
